6a7687d26cc6fbbe61afee4596db36fec324d34f
Thomas Capelle <tcapelle@pm.me>
2024-08-21 17:43:25 +0200

    my vllm server

--- providers.py
+++ providers.py
@@ -35,5 +35,12 @@ PROVIDERS = {
             base_url="https://api.together.xyz/v1",
         ),
         model="meta-llama/Meta-Llama-3.1-70B-Instruct-Turbo"
+    ),
+    "vllm_8xh100": Provider(
+        client=openai.OpenAI(
+            api_key="EMPTY",
+            base_url="http://195.242.16.86:8000/v1",
+        ),
+        model="meta-llama/Meta-Llama-3.1-70B-Instruct"
     )
 }
