3249f7266147d50ff2881c887f99b10277922d3c
Thomas Capelle <tcapelle@pm.me>
2024-08-27 18:34:04 +0200

    update code

--- benchmark_bis.py
+++ benchmark_bis.py
@@ -2,15 +2,12 @@ from dataclasses import dataclass
 import openai
 import random
 import time
-import weave
-from weave.autopatch import reset_autopatch
 import asyncio
-from providers import PROVIDERS
-
+from tqdm import tqdm
 import simple_parsing
 
+from providers import PROVIDERS
 
-@weave.op
 def call_llama(client, model="llama3.1-70b", prompt=None):
     start_time = time.perf_counter()
     response = client.chat.completions.create(
@@ -23,52 +20,55 @@ def call_llama(client, model="llama3.1-70b", prompt=None):
     )
     end_time = time.perf_counter()
     real_time = end_time - start_time
-    
     completion_tok_sec = response.usage.completion_tokens / real_time
 
     return {
         "real_time": real_time, 
         "completion_tok_sec": completion_tok_sec, 
-        "real_time": real_time,
-        "total_tokens": response.usage.total_tokens,
         "response": response
     }
 
-def metrics(model_output):
-    return {
-        "real_time": model_output["real_time"],
-        "completion_tok_sec": model_output["completion_tok_sec"],
-        "total_tokens": model_output["total_tokens"],
+
+def evaluate_model_perf(client, model, prompt, num_runs=10):
+    perf_results = {
+        "real_time": 0., 
+        "completion_tok_sec": 0., 
     }
+    for i in tqdm(range(num_runs)):
+        output = call_llama(client, model=model, prompt=prompt)
+        perf_results["real_time"] += output["real_time"]
+        perf_results["completion_tok_sec"] += output["completion_tok_sec"]
+    # compute averages
+    perf_results["real_time"] /= num_runs
+    perf_results["completion_tok_sec"] /= num_runs
+    return perf_results
 
 
 def main(config):
     provider = PROVIDERS[config.provider]
-    weave.init(config.weave_project)
-    reset_autopatch()
 
-    class Llama70b(weave.Model):
-        @weave.op
-        def predict(self, prompt):
-            return call_llama(provider.client, provider.model, prompt)
+    perf_results = evaluate_model_perf(provider.client, provider.model, config.prompt, config.n)
+    print(perf_results)
 
-    llama = Llama70b()
-    evaluation = weave.Evaluation(dataset=[{"prompt": config.prompt} for _ in range(config.n)], scorers=[metrics])
+    
 
-    asyncio.run(evaluation.evaluate(llama))
 
 @dataclass
 class BenchmarkConfig:
     provider: str = "fireworks" # provider to evaluate
     prompt: str = "Tell me a long story about a cat" # prompt to evaluate
     n: int = 10 # number of prompts to evaluate
-    weave_project: str = "benchmark_llama_70b" # "prompt-eng/benchmark_llama_70b"
+    use_weave: bool = False # whether to use weave to evaluate the model
 
 
 
 if __name__ == "__main__":
     config = simple_parsing.parse(BenchmarkConfig)
     print(f"Running benchmark with config: {config}")
+    
+    if config.use_weave:
+        import weave
+        weave.init("overhead")
     main(config)
 
 
--- providers.py
+++ providers.py
@@ -9,42 +9,42 @@ class Provider:
 
 PROVIDERS = {
     "cerebras": Provider(
-        client=openai.AsyncOpenAI(
+        client=openai.OpenAI(
             base_url="https://api.cerebras.ai/v1/", 
             api_key=os.getenv("CEREBRAS_API_KEY")
         ),
         model="llama3.1-70b"
     ),
     "octo": Provider(
-        client=openai.AsyncOpenAI(
+        client=openai.OpenAI(
             base_url="https://text.octoai.run/v1",
             api_key=os.environ.get("OCTO_API_KEY")
         ),
         model="meta-llama-3.1-70b-instruct"
     ),
     "groq": Provider(
-        client=openai.AsyncOpenAI(
+        client=openai.OpenAI(
             base_url="https://api.groq.com/openai/v1",
             api_key=os.environ.get("GROQ_API_KEY")
         ),
         model="llama-3.1-70b-versatile"
     ),
     "fireworks": Provider(
-        client=openai.AsyncOpenAI(
+        client=openai.OpenAI(
             base_url="https://api.fireworks.ai/inference/v1", 
             api_key=os.getenv("FIREWORKS_API_KEY")
         ),
         model="accounts/fireworks/models/llama-v3p1-70b-instruct"
     ),
     "together": Provider(
-        client=openai.AsyncOpenAI(
+        client=openai.OpenAI(
             api_key=os.environ.get("TOGETHER_API_KEY"),
             base_url="https://api.together.xyz/v1",
         ),
         model="meta-llama/Meta-Llama-3.1-70B-Instruct-Turbo"
     ),
     "openai": Provider(
-        client=openai.AsyncOpenAI(
+        client=openai.OpenAI(
             api_key=os.environ.get("OPENAI_API_KEY"),
         ),
         model="gpt-4o-mini"
